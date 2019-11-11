23ca8cf13601c0c04dfd1bf4afc4d6856f148068
Wen1224 <55408567+Wen1224@users.noreply.github.com>
2019-11-11 16:57:08 -0500

    Add files via upload

---
+++ matthews.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import copy
+import gc
+import os
+import sys
+import warnings
+
+from IPython.core.display import display, HTML
+import lightgbm as lgb
+from matplotlib import pyplot as plt
+import numba
+import numpy as np
+import pandas as pd
+import pyarrow
+import pyarrow.parquet as pq
+import scipy.stats
+import seaborn as sns 
+import sklearn
+from sklearn.metrics import matthews_corrcoef, precision_recall_fscore_support
+
+
+# In[2]:
+
+
+def mcc(tp, tn, fp, fn):
+    sup = tp * tn - fp * fn
+    inf = (tp + fp) * (tp + fn) * (tn + fp) * (tn + fn)
+    if inf==0:
+        return 0
+    else:
+        return sup / np.sqrt(inf)
+
+
+def eval_mcc(y_true, y_prob, show=False):
+    idx = np.argsort(y_prob)
+    y_true_sort = y_true[idx]
+    n = y_true.shape[0]
+    nump = 1.0 * np.sum(y_true) # number of positive
+    numn = n - nump # number of negative
+    tp = nump
+    tn = 0.0
+    fp = numn
+    fn = 0.0
+    best_mcc = 0.0
+    best_id = -1
+    prev_proba = -1
+    best_proba = -1
+    mccs = np.zeros(n)
+    for i in range(n):
+        # all items with idx < i are predicted negative while others are predicted positive
+        # only evaluate mcc when probability changes
+        proba = y_prob[idx[i]]
+        if proba != prev_proba:
+            prev_proba = proba
+            new_mcc = mcc(tp, tn, fp, fn)
+            if new_mcc >= best_mcc:
+                best_mcc = new_mcc
+                best_id = i
+                best_proba = proba
+        mccs[i] = new_mcc
+        if y_true_sort[i] == 1:
+            tp -= 1.0
+            fn += 1.0
+        else:
+            fp -= 1.0
+            tn += 1.0
+    if show:
+        y_pred = (y_prob >= best_proba).astype(int)
+        score = matthews_corrcoef(y_true, y_pred)
+        plt.plot(mccs)
+        return best_proba, best_mcc, y_pred
+    else:
+        return best_proba,  best_mcc, None
+
+
+# In[5]:
+
+
+def matthews_correlation(y_true, y_pred):
+    y_true = K.cast(y_true, float)
+    y_pred = K.cast(y_pred, float)
+
+    y_pred_pos = K.round(K.clip(y_pred, 0, 1))
+    y_pred_neg = 1 - y_pred_pos
+
+    y_pos = K.round(K.clip(y_true, 0, 1))
+    y_neg = 1 - y_pos
+
+    tp = K.sum(y_pos * y_pred_pos)
+    tn = K.sum(y_neg * y_pred_neg)
+
+    fp = K.sum(y_neg * y_pred_pos)
+    fn = K.sum(y_pos * y_pred_neg)
+
+    numerator = (tp * tn - fp * fn)
+    denominator = K.sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
+
+    return numerator / (denominator + K.epsilon())
+
